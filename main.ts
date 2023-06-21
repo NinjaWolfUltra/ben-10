6d17e72e5c4dea660b3afc01f469f5b3f26be6fb
NinjaWolfUltra <136949535+NinjaWolfUltra@users.noreply.github.com>
2023-06-21 15:27:56 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f . . . . . . . . . 
+    . . f f f f e f . . . . . . . . 
+    . f e e e f e e f f f . . . . . 
+    . . f e e e e e e e e f . . . . 
+    . . f e e e e e e e e e f . . . 
+    . f e e e e e e e e e e e f . . 
+    . f f e f 7 f 4 4 f 7 f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 1 f 1 1 f 1 f 4 e . . 
+    . . f 7 f 1 f 1 1 f 1 f 4 4 . . 
+    . . 7 f f 1 f 1 1 f 1 f d 4 . . 
+    . . 4 4 . 6 6 6 6 6 6 . 4 4 . . 
+    . . . . . 6 6 . . 6 6 . . . . . 
+    `, SpriteKind.Player)
